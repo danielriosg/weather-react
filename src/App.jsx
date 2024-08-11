15b6142f97388a45d6737555ac57461e049f6e1e
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-08-11 14:20:45 -0400

    Update footer.jsx, css and create import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -17,6 +18,7 @@ function App() {
           <Route path='/contact' element={<Contact />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../components/Footer";
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <p>&copy; 2024 My Weather App. All rights reserved.</p>
+      <p>Contact: info@myweatherapp.com</p>
+    </footer>
+  );
+};
+
+export default Footer;

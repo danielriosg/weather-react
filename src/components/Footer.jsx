db17b3aea8c0b383ace5f01392a8fef614932356
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-08-21 12:54:29 -0400

    Adjust footer size and add github repo link with logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import "../components/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import "../styles/components/Footer.css";
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <p>&copy; 2024 My Weather App. All rights reserved.</p>
       <p>Contact: info@myweatherapp.com</p>
+      <p>
+        View the code on GitHub:
+        <a
+          href='https://github.com/danielriosg/weather-react'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='github-link'
+        >
+          <FontAwesomeIcon icon={faGithub} size='2x' />
+        </a>
+      </p>
     </footer>
   );
 };
 
 export default Footer;
+
+
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/components/Header.css"; 
+import "../styles/components/Header.css";
 import DarkModeToggle from "./DarkModeToggle";
 
 function Header() {

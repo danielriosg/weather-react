2e9250e765cb5076f93e9db5605f44b32a9a4197
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-08-24 14:28:41 -0400

    Update package.json with new scripts to deploy in gh

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/weather-react/", // Update this to match your repository
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
-})
+});
+

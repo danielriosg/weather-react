d7f77372d2198bd8cf17d259372e7a59012886a6
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-08-25 20:34:55 -0400

    Update routing with basename and default route for Github deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css"; // Importing the global CSS
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/weather-react'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weather-react/", // Update this to match your repository
+  base: "", // Update this to match your repository
   build: {
     outDir: "dist",
   },
